4142bb1999ebcc3fac4c41da634f81b3e7b5af92
xujian <1427302352@qq.com>
2016-09-28 23:09:09 +0800

    program getState,dispatch,subscribe(unsubscribe) of createStore on my own.

--- src/app.js
+++ src/app.js
@@ -1,21 +1,67 @@
-function counter(state, action) {
-    switch(action.type) {
+// import {createStore} from 'redux';
+
+const counter = (state = 0, action) => {
+    switch (action.type) {
         case 'INCREASE':
             return state + 1;
         case 'DECREASE':
             return state - 1;
-        default: 
+        default:
             return state;
     }
 }
 
-console.log('current state is 0');
-console.log('trigger INCREASE,current state is');
-console.log(counter(0, {
-    type: 'INCREASE'
-}));
-console.log('current state is 2');
-console.log('trigger DECREASE,current state is');
-console.log(counter(2, {
-    type: 'DECREASE'
-}))
+const createStore = (reducer) => {
+    let state;
+    let list = [];
+    const getState = () => {
+        return state;
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        list.forEach((fn) => {
+            fn();
+        })
+    }
+
+    const subscribe = (fn) => {
+        list.push(fn);
+        return () => {
+            list = list.filter(cb => cb !== fn);
+        };     
+    }
+
+    return {
+        getState,
+        dispatch,
+        subscribe
+    }
+}
+
+const store = createStore(counter);
+store.dispatch({
+    type: 'INIT'
+})
+
+document.addEventListener('click', () => {
+    store.dispatch({
+        type: 'INCREASE'
+    })
+    unsubscribe1();
+})
+
+const render1 = () => {
+    console.log('click event triggered!!!')
+    // document.write('<h1>' + store.getState() + '</h1>');//会把事件清除掉！！！
+    document.body.innerHTML = '<h1>' + store.getState() + '</h1>';
+};
+
+const render2 = () => {
+    console.log('trigger render2');
+}
+
+var unsubscribe1 = store.subscribe(render1);
+var unsubscribe2 = store.subscribe(render2);
+
+render1();
