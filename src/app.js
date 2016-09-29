bb96f676a5a960d13daaef0ed5bf431f106e7b99
xujian <1427302352@qq.com>
2016-09-29 21:24:54 +0800

    react and react-dom operate the function

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-// import {createStore} from 'redux';
+import {createStore} from 'redux';
+import React, {Component} from 'react';
+import ReactDom from 'react-dom';
 
 const counter = (state = 0, action) => {
     switch (action.type) {
@@ -11,57 +13,35 @@ const counter = (state = 0, action) => {
     }
 }
 
-const createStore = (reducer) => {
-    let state;
-    let list = [];
-    const getState = () => {
-        return state;
-    }
-
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        list.forEach((fn) => {
-            fn();
-        })
-    }
-
-    const subscribe = (fn) => {
-        list.push(fn);
-        return () => {
-            list = list.filter(cb => cb !== fn);
-        };     
-    }
-
-    return {
-        getState,
-        dispatch,
-        subscribe
-    }
-}
-
 const store = createStore(counter);
-store.dispatch({
-    type: 'INIT'
-})
 
-document.addEventListener('click', () => {
-    store.dispatch({
-        type: 'INCREASE'
-    })
-    unsubscribe1();
-})
-
-const render1 = () => {
-    console.log('click event triggered!!!')
-    // document.write('<h1>' + store.getState() + '</h1>');//会把事件清除掉！！！
-    document.body.innerHTML = '<h1>' + store.getState() + '</h1>';
-};
-
-const render2 = () => {
-    console.log('trigger render2');
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{store.getState()}</h1>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'DECREASE'
+                    })
+                }}>-</button>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'INCREASE'
+                    })
+                }}>+</button>
+            </div>
+        )
+    }
 }
 
-var unsubscribe1 = store.subscribe(render1);
-var unsubscribe2 = store.subscribe(render2);
+const render = () => {
+    ReactDom.render(
+        <Counter/>,
+        document.getElementById('root')
+    )
+}
 
-render1();
+render();
+
+const unsubscribe = store.subscribe(render);
