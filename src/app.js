086d44746ae8ce381fb4cbc0921927c44baf7942
xujian <1427302352@qq.com>
2016-10-02 17:52:52 +0800

    init

--- src/app.js
+++ src/app.js
@@ -2,35 +2,6 @@ import {createStore} from 'redux';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
-let gid = 0;
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            // let newState = [].concat(state);
-            // return newState.push({
-            //     id: ++gid,
-            //     text: action.text,
-            //     completed: false
-            // });
-
-            return [...state, {
-                id: ++gid,
-                text: action.text,
-                completed: false
-            }]
-        case 'TOGGLE_TODO':
-            return state.map((todo) => {
-                if (todo.id == action.id) {
-                    return Object.assign({}, todo, {
-                        completed: !todo.completed
-                    })
-                }
-                return todo;
-            });
-        default:
-            return state;
-    }
-}
 
 let store = createStore(todos);
 
