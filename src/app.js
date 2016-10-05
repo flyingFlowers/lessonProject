a8ca55fda0154c4ab51616efc207a25c2d6921e5
xujian <1427302352@qq.com>
2016-10-05 22:07:25 +0800

    redux provide the Provider of function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {createStore} from 'redux';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
+import {Provider} from 'react-redux';
 
 import rootReducer from './reducers/index.js';
 import FilterLink from './components/FilterLink.js';
@@ -9,55 +10,45 @@ import TodoList from './components/TodoList.js';
 import Footer from './components/Footer.js';
 const store = createStore(rootReducer);
 
-const getVisibleTodos = (todos, visiableFilter) => {
-    switch (visiableFilter) {
-        case 'SHOW_ALL':
-            return todos;
-        case 'SHOW_COMPLETED':
-            return todos.filter(todo => todo.completed)
-        case 'SHOW_ACTIVE':
-            return todos.filter(todo => !todo.completed)
-        default:
-            throw new Error('unknow filter');
-    }
-}
-
 class App extends Component {
     render() {
         let {todos, visiableFilter} = store.getState();
-        todos = getVisibleTodos(todos, visiableFilter);
         return (
             <div>
-                <AddTodo handleClick={(value) => {
-                    store.dispatch({
-                        type: 'ADD_TODO',
-                        text: value
-                    })
-                } }/>
-                <TodoList todos={todos} handleClick={(id) => {
-                    console.log('trigger TodoList')
-                    store.dispatch({
-                        type: 'TOGGLE_TODO',
-                        id: id
-                    })
-                } }/>
-                <Footer currentFilter={visiableFilter} handleClick={(filter) => {
-                    store.dispatch({
-                        type: 'SET_VISIABLEFILTER',
-                        filter
-                    })
-                } }/>
+                <AddTodo/>
+                <TodoList/>
+                <Footer/>
             </div>
         )
     }
 }
 
+// class Provider extends Component {
+//     getChildContext() {
+//         return {
+//             store: this.props.store
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             this.props.children
+//         )
+//     }
+// }
+
+// Provider.childContextTypes = {
+//     store: React.PropTypes.object
+// }
+
 const render = () => {
     ReactDom.render(
-        <App />,
+        <Provider store={store}>
+            <App/>
+        </Provider>,
         document.getElementById("root")
     )
 }
 render();
 
-store.subscribe(render);
+// store.subscribe(render);
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 
 class AddTodo extends Component {
+    componentDidMount() {
+        const store = this.context.store;
+        this.unsubscribe = store.subscribe(() => {
+            this.forceUpdate();
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
     render() {
+        const store = this.context.store;
+        const handleClick = (value) => {
+            store.dispatch({type: 'ADD_TODO', text: value})
+        }
         return (
             <div>
                 <input type="text" ref="_Inp"></input>
-                <button onClick={() => {
-                    this.props.handleClick(this.refs._Inp.value);
+                <button
+                    onClick={() => {
+                    handleClick(this.refs._Inp.value);
                 }}>ADD</button>
-            </div>    
+            </div>
         )
     }
 }
 
+AddTodo.contextTypes = {
+    store: React.PropTypes.object
+}
 export default AddTodo;
