a42ce731419cf047a01637fa04a9420be55c1346
xujian <1427302352@qq.com>
2016-10-24 14:16:33 +0800

    想去看看dispatch怎么写的

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import {createStore} from 'redux';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
-import store from './configureStore.js';
+import configureStore from './configureStore.js';
 import Root from './components/Root.js';
 
 const render = () => {
     ReactDom.render(
-        <Root store={store}/>, document.getElementById("root"))
+        <Root store={configureStore()}/>, document.getElementById("root"))
 }
 render();
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,24 @@
-import {createStore} from 'redux';
-import {throttle} from 'lodash'; //执行某个函数时，规定固定时间才能执行一次
+import { createStore } from 'redux';
+import { throttle } from 'lodash'; //执行某个函数时，规定固定时间才能执行一次
 
-import {saveState, getState} from './localStorage.js';
+import { saveState, getState } from './localStorage.js';
 import rootReducer from './reducers/index.js';
-const mockData = getState();
-let store = createStore(rootReducer, mockData);
-store.subscribe(throttle(() => {
-    saveState(store.getState());
-}, 1000)); //1000ms才能执行一次
 
-export default store;
+const addLoggerToDispatch = (store) => {
+    const rawDispatch = store.dispatch;
+    return (action) => {
+        console.log('-------------------');
+        console.log('Current state', store.getState());
+        console.log('Action:', action);
+        const ret = rawDispatch(action);
+        console.log('Next state:', store.getState());
+        return ret;
+    }
+}
+
+const configureStore = () => {
+    let store = createStore(rootReducer);
+    store.dispatch = addLoggerToDispatch(store);
+    return store;
+}
+export default configureStore;
